5889a008e10f79c0bc613e83aa574626b51e895e
woooyoung <axdsw121@gmail.com>
2021-12-19 16:52:40 +0900

    게시물 요구사항3 - 삭제 구현 전 중복되는 코드 및 복잡한 코드 정리

--- BoardMain.java
+++ BoardMain.java
@@ -4,76 +4,35 @@ import java.util.Scanner;
 
 public class BoardMain {
 
+	static Scanner scan = new Scanner(System.in);
+	static int[] ids = new int[3]; // 번호 저장소
+	static String[] titles = new String[3]; // 제목 저장소
+	static String[] bodies = new String[3]; // 내용 저장소
+	static int lastIndex = 0; // 데이터 하나 저장할 때마다 1증가
+	
 	public static void main(String[] args) {
-		
-		Scanner scan = new Scanner(System.in);
-		int[] ids = new int[3]; // 번호 저장소
-		String[] titles = new String[3]; // 제목 저장소
-		String[] bodies = new String[3]; // 내용 저장소
-		int lastIndex = 0; // 데이터 하나 저장할 때마다 1증가
-		//int articleId = 1; // 게시물 번호
 		
 		while(true) {
 			System.out.print("명령어를 입력해주세요 : ");
 			String cmd = scan.nextLine();
 			
 			if(cmd.equals("list")) {
-				
-				for(int i = 0; i < lastIndex; i++) {		
-					System.out.println("번호 : " + ids[i]);
-					System.out.println("제목 : " + titles[i]);
-					System.out.println("=====================");
-				}
+				list();
 				
 			}
-			else if(cmd.equals("update")) {
-				System.out.print("수정할 게시물 번호 : ");
-				int targetId = Integer.parseInt(scan.nextLine());
-				
-				int index = -1;
-				
-				for(int i = 0; i < lastIndex; i++) {
-					if(targetId == ids[i]) {						
-						index = i;
-						break;
-					}
-				}
-				
-				if(index == -1) {
-					System.out.println("없는 게시물 번호입니다.");
-				} else {
-					System.out.print("제목 : ");
-					String title = scan.nextLine();
-					System.out.print("내용 : ");
-					String body = scan.nextLine();
-					
-					titles[index] = title;
-					bodies[index] = body;
-					
-					System.out.println("게시물 수정이 완료되었습니다.");
-					
-					for(int i = 0; i < lastIndex; i++) {		
-						System.out.println("번호 : " + ids[i]);
-						System.out.println("제목 : " + titles[i]);
-						System.out.println("=====================");
-					}
-				}
+			else if(cmd.equals("delete")) {
+				delete();
+			}
+			else if(cmd.equals("update")) {		
+				update();
 				
 			}	
 			else if(cmd.equals("add")) {			
+				add();
 				
-				ids[lastIndex] = lastIndex + 1;
-				System.out.print("제목을 입력해주세요 : ");
-				titles[lastIndex] = scan.nextLine();
-				System.out.print("내용을 입력해주세요 : ");
-				bodies[lastIndex] = scan.nextLine();
-				
-				System.out.println("게시물이 저장되었습니다.");
-				lastIndex++;
 			}
 			else if(cmd.equals("help")) {
-				System.out.println("add  : 게시물 등록");
-				System.out.println("list : 게시물 목록 조회");		
+				printHelp();	
 				
 			} 
 			else {
@@ -81,6 +40,81 @@ public class BoardMain {
 			}
 		}
 		
+	}
+
+	private static void delete() {
+		
+		System.out.print("삭제할 게시물 번호 : ");
+		int targetId = Integer.parseInt(scan.nextLine());
+		
+		int index = findIndexByArticleId(targetId);
+		
+		if(index == -1) {
+			System.out.println("없는 게시물 번호입니다.");
+		} else {
+			// 삭제 코드 작성
+			System.out.println("삭제 기능 실행");
+		}
+	}
+
+	private static void printHelp() {
+		System.out.println("add  : 게시물 등록");
+		System.out.println("list : 게시물 목록 조회");	
+	}
+
+	private static void add() {
+		ids[lastIndex] = lastIndex + 1;
+		System.out.print("제목을 입력해주세요 : ");
+		titles[lastIndex] = scan.nextLine();
+		System.out.print("내용을 입력해주세요 : ");
+		bodies[lastIndex] = scan.nextLine();
+		
+		System.out.println("게시물이 저장되었습니다.");
+		lastIndex++;
+	}
+
+	private static void update() {
+		System.out.print("수정할 게시물 번호 : ");
+		int targetId = Integer.parseInt(scan.nextLine());
+		
+		int index = findIndexByArticleId(targetId);
+		
+		if(index == -1) {
+			System.out.println("없는 게시물 번호입니다.");
+		} else {
+			System.out.print("제목 : ");
+			String title = scan.nextLine();
+			System.out.print("내용 : ");
+			String body = scan.nextLine();
+			
+			titles[index] = title;
+			bodies[index] = body;
+			
+			System.out.println("게시물 수정이 완료되었습니다.");
+			list();
+		}
+		
+	}
+
+	private static void list() {
+		for(int i = 0; i < lastIndex; i++) {		
+			System.out.println("번호 : " + ids[i]);
+			System.out.println("제목 : " + titles[i]);
+			System.out.println("=====================");
+		}	
+	}
+	
+	public static int findIndexByArticleId(int targetId) {
+		int index = -1;
+		
+		for(int i = 0; i < lastIndex; i++) {
+			if(targetId == ids[i]) {						
+				index = i;
+				break;
+			}
+		}
+		
+		return index;
 	}
 
 }
